6deb525c4a500874b8eaaaccc639d2d690f0d8e0
Milson Fortunato Neto <milsonfn@yahoo.com>
2023-07-14 14:57:57 -0300

    changes

---
+++ JAVASCRIPT/arquivo.js
@@ -0,0 +1,16 @@
+/*
+Variáveis
+
+*/
+const users = [
+    {
+        name: "Zequinha", age: 26, id: null
+
+    },
+    {
+        name: "Joana", age: 45, id: 458779
+
+    }
+]
+
+console.log(users)
