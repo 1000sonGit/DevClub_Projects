b3454deaa6c7ba80834f3bef12f17798905a5822
Milson Fortunato Neto <milsonfn@yahoo.com>
2023-07-17 13:32:08 -0300

    following

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button } from "./styles"
 
 const App = () => {
-  
-  return <div>Hello World</div>
+
+  return (<Container>
+    <Image/>
+    <ContainerItens>
+      <H1>Olá!</H1>
+      <InputLabel>Nome</InputLabel>
+      <Input placeholder="Nome"/>
+
+      <InputLabel>Idade</InputLabel>
+      <Input placeholder="Idade"/>
+
+      <Button>Cadastrar</Button>
+
+    </ContainerItens>
+  </Container>)
 }
 
 export default App
