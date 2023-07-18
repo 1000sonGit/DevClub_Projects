c459dda44cd2f6f61f5b122fbd0d2d14e2bd8173
Milson Fortunato Neto <milsonfn@yahoo.com>
2023-07-18 13:27:14 -0300

    newmodif

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,25 +1,42 @@
 import React from "react";
-import { Container, H1, Image, ImageArrow, ContainerItens, InputLabel, Input, Button } from "./styles"
+import {
+  Container, 
+  H1, 
+  Image,
+  ContainerItens, 
+  InputLabel, 
+  Input, 
+  Button,
+  User} from "./styles"
 import People from './assets/people.svg'
 import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
 
 const App = () => {
 
-  const users = [{id: Math.random(), name:'Milson', age:39}, {id: Math.random(), name:'Maria', age:26}]
+  const users = [
+    { id: Math.random(), name: 'Milson', age: 39 },
+    { id: Math.random(), name: 'Maria', age: 26 },
+  ];
 
   return (<Container>
-    <Image alt='logo-image' src={People}/>
+    <Image alt='logo-image' src={People} />
     <ContainerItens>
       <H1>Olá!</H1>
       <InputLabel>Nome</InputLabel>
-      <Input placeholder="Nome"/>
+      <Input placeholder="Nome" />
 
       <InputLabel>Idade</InputLabel>
-      <Input placeholder="Idade"/>
+      <Input placeholder="Idade" />
 
-      <Button> Cadastrar <ImageArrow alt="seta" src={Arrow}/></Button>
+      <Button> Cadastrar <img alt="seta" src={Arrow} /></Button>
       <ul>
-        <li>{users[0].name} - {users[0].age}</li>
+        {users.map((user) => (
+          <User key={user.id}>
+            <p>{user.name}</p> <p>{user.age}</p>
+            <button><img alt="trash" src={Trash}/></button>
+          </User>
+        ))}
       </ul>
     </ContainerItens>
   </Container>)
--- REACT/my-app/src/styles.js
+++ REACT/my-app/src/styles.js
@@ -16,12 +16,6 @@ export const Container = styled.div`
 export const Image = styled.img`
     margin-top: 30px;    
 `
-
-export const ImageArrow = styled.img`
-    width:22.95px;
-    height:18.93px;  
-`
-
 export const ContainerItens = styled.div`
 
     background: linear-gradient(
@@ -83,7 +77,7 @@ export const Input = styled.input`
 `
 
 export const Button = styled.button`
-    width: 342px;
+    width: 367px;
     height: 74px;
 
     background: rgba(0, 0, 0, 0.8);
@@ -104,6 +98,11 @@ export const Button = styled.button`
     justify-content: center;
     gap: 20px;
 
+    img {
+        width:22.95px;
+        height:18.93px;
+    }
+
     &:hover{
         opacity: 0.8;
     }
@@ -112,4 +111,36 @@ export const Button = styled.button`
         opacity: 0.5;
     }
    
+`
+
+export const User = styled.li`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+    width: 367px;
+    height: 58px;
+
+    border: none;
+    outline: none;
+
+    p {
+        font-style: normal;
+        font-weight: normal;
+        font-size: 24px;
+        line-height: 28px;
+
+        color: #ffffff;
+    }
+
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;        
+    }
+
 `
