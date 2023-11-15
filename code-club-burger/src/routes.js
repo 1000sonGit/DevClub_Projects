e983e284646bd789a5eb29399c766e990f9754e9
Milson Fortunato Neto <milsonfn@yahoo.com>
2023-11-15 12:45:58 -0300

    AtAll

--- code-club-burger/src/routes.js
+++ code-club-burger/src/routes.js
@@ -1,9 +1,32 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
+
+import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import ProductController from './app/controllers/ProductController'
+import CategoryController from './app/controllers/CategoryController'
+
+import authMiddleware from './app/middlewares/auth'
+import OrderController from './app/controllers/OrderController'
+
+const upload = multer(multerConfig)
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+routes.post('/users', UserController.store)
+
+routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware) // será chamdo por todas as rotas abaixo
+
+routes.post('/products', upload.single('file'), ProductController.store)
+routes.get('/products', ProductController.index)
+
+routes.post('/categories', CategoryController.store)
+routes.get('/categories', CategoryController.index)
+
+routes.post('/orders', OrderController.store)
+routes.get('/orders', OrderController.index)
 
 export default routes
